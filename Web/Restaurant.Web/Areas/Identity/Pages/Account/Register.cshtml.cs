618c36e7872e4e17e6259f7ed06869adf4d36594
PanayotPetrov <panayot.petrov20@gmail.com>
2022-02-22 22:38:17 +0200

    Changed login to use username instead of email

--- Web/Restaurant.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Web/Restaurant.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,7 +46,8 @@ namespace Restaurant.Web.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            public string Username { get; set; }
+            [Required]
+            public string UserName { get; set; }
 
             [Required]
             [EmailAddress]
@@ -86,7 +87,7 @@ namespace Restaurant.Web.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName };
+                var user = new ApplicationUser { UserName = Input.UserName, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
