161a8589517e4e0216b386eab511b33c084ddc66
PanayotPetrov <panayot.petrov20@gmail.com>
2023-08-31 14:00:21 +0300

    Revert "Adding a new project - AdminDashboard.Web"
    This reverts commit 44dd5a3453e18656f5e39f4f869ac85b2226b87e.

--- AdminDashboard.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AdminDashboard.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
