9977f2755adb7c19ef2e9431825dfa9d517638bc
PanayotPetrov <panayot.petrov20@gmail.com>
2023-12-01 15:01:51 +0200

    Adding additional properties to Meal entity in order to support localization

---
+++ Data/Restaurant.Data/Migrations/20231201125854_AddingSecondaryCultureSupportToMealEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurant.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingSecondaryCultureSupportToMealEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SecondaryDescription",
+                table: "Meals",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: string.Empty);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SecondaryDescription",
+                table: "Meals");
+        }
+    }
+}
